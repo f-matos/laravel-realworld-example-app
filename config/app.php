1a0da0cb0ed3a902262623b3c33d6aca6ba8ef71
MEGAZORD <sazuna@users.noreply.github.com>
2018-06-02 12:27:01 -0300

    Fixes for PHP 7.2

--- config/app.php
+++ config/app.php
@@ -168,7 +168,7 @@ return [
          */
         Laravel\Tinker\TinkerServiceProvider::class,
         Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
-        Barryvdh\Cors\ServiceProvider::class,
+        //Barryvdh\Cors\ServiceProvider::class,
 
         /*
          * Application Service Providers...
